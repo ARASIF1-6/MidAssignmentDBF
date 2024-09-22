1830c818080a44490c9f265da4c4a60d5890b19a
ARASIF1-6 <asifrahman1835@gmail.com>
2024-09-23 03:33:55 +0600

    Upload Code

---
+++ MidAssignment/Controllers/DepartmentController.cs
@@ -0,0 +1,51 @@
+﻿using MidAssignment.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MidAssignment.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        EmployeeDatabaseEntitiesContext _dbContext;
+        public DepartmentController()
+        {
+            _dbContext = new EmployeeDatabaseEntitiesContext();
+        }
+
+        // GET: Department
+        public ActionResult Index()
+        {
+            var departmentList = _dbContext.Departments.ToList();
+            return View(departmentList);
+        }
+
+        [HttpGet]
+        public ActionResult AddDepartment()
+        {
+                ModelState.Clear();
+                return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddDepartment(Department d) // Action method overloading
+        {
+                ModelState.Remove("Id");
+                if (ModelState.IsValid)
+                {
+                    _dbContext.Departments.Add(d); // Save employee
+                    _dbContext.SaveChanges();
+                    TempData["MsgAdd"] = "Department information added successfully";
+
+                    return RedirectToAction("Index");
+                }
+
+            ModelState.Clear();
+            return View("AddDepartment");
+
+        }
+    }
+}
---
+++ MidAssignment/Controllers/EmployeeController.cs
@@ -0,0 +1,83 @@
+﻿using MidAssignment.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MidAssignment.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDatabaseEntitiesContext _dbContext;
+        public EmployeeController()
+        {
+            _dbContext = new EmployeeDatabaseEntitiesContext();
+        }
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employeeList = _dbContext.Employees.ToList();
+            return View(employeeList);
+        }
+
+        [HttpGet]
+        public ActionResult AddEmployee(Employee e)
+        {
+            if (e.Id > 0)
+            {
+                return View(e);
+            }
+            else
+            {
+                ModelState.Clear();
+                ViewBag.NoData = 0;
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateEmployee(Employee e)
+        {
+            if (e.Id <= 0)
+            {
+                ModelState.Remove("Id");
+                if (ModelState.IsValid)
+                {
+                    _dbContext.Employees.Add(e); // Save employee
+                    _dbContext.SaveChanges();
+                    TempData["MsgAdd"] = "Employee information added successfully";
+
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                _dbContext.Entry(e).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+                TempData["MsgMod"] = "Employee information modified successfully";
+
+                return RedirectToAction("Index");
+
+
+            }
+
+            ModelState.Clear();
+            return View("AddEmployee");
+
+        }
+
+        public ActionResult DeleteEmployee(int id)
+        {
+
+            var delete_employee = _dbContext.Employees.Where(x => x.Id == id).First();
+            _dbContext.Employees.Remove(delete_employee);
+            _dbContext.SaveChanges();
+            TempData["MsgRem"] = "Employee information removed successfully";
+
+            return RedirectToAction("Index");
+        }
+    }
+}
